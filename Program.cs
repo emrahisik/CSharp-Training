dd3575c4b0916af3e4abf07f0214eda9ed1e47df
emrahisik <emrahisk@gmail.com>
2024-01-03 22:38:28 -0500

    Lists, IEnumarables, 2D arrays, Dictionaries

--- Program.cs
+++ Program.cs
@@ -63,10 +63,26 @@ namespace HelloWorld
       // myFruits[2] = "";
       Console.WriteLine(myFruits[0]);
       Console.WriteLine(myFruits[1]);
+      Console.WriteLine(myFruits.GetType());
       // Console.WriteLine(myFruits[2]);
 
       // Lists
-      List<string> myList = []; // Old version => List<string> myList = new List<string>()
+      List<string> myGroceryList = ["Asparagus", "Milk", "Eggs"]; // Old version => List<string> myList = new List<string>()
+      Console.WriteLine($"{myGroceryList[0]}, {myGroceryList[2]}");
+      myGroceryList.Add("Tomatoes");
+      Console.WriteLine($"{myGroceryList[0]}, {myGroceryList[3]}");
+
+      // IEnumerable
+      IEnumerable<string> myGroceryIEnum = myGroceryList; 
+      Console.WriteLine(myGroceryIEnum.Last());
+
+      // 2D array
+      string[,] myTwoDimentionalArray = {{"apples", "pears", "bananas"},{"eggs", "milk", "cheese"}};
+      Console.WriteLine (myTwoDimentionalArray[1,2]);
+
+      // Dictionary
+      Dictionary<string,string[]> groceryMap = new(){{"Diary", new string[]{"Cheeese", "Milk", "Eggs"}}}; // Old version = new Dictionary<string, string[]>(){{"Diary", new string[]{"Cheeese", "Milk"}}};
+      Console.WriteLine(groceryMap["Diary"][2]);
     }
   }
 }
