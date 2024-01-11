8b210e9b32ee6d62cd6ae65275f886a36f62c07d
emrahisik <emrahisk@gmail.com>
2024-01-10 22:58:46 -0500

    Conditionals

--- Program.cs
+++ Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
+using System.Diagnostics;
+using Microsoft.VisualBasic;
 namespace HelloWorld
 {
   internal class Program
@@ -8,11 +10,40 @@ namespace HelloWorld
     {
       int firstInt = 5;
       int secondInt = 10;
-      Console.WriteLine(firstInt.Equals(secondInt)); // returns true || flase 
-      Console.WriteLine(firstInt.CompareTo(secondInt)); // returns 0 (equal)|| 1 (greater than) || -1 (less than)
-      //Shoerthand
-      Console.WriteLine(firstInt == secondInt);
-      Console.WriteLine(firstInt < secondInt);
+      // Console.WriteLine(firstInt.Equals(secondInt)); // returns true || flase 
+      // Console.WriteLine(firstInt.CompareTo(secondInt)); // returns 0 (equal)|| 1 (greater than) || -1 (less than)
+      // //Shoerthand
+      // Console.WriteLine(firstInt == secondInt);
+      // Console.WriteLine(firstInt < secondInt);
+      
+      if(firstInt > secondInt)
+      {
+        Console.WriteLine($"{firstInt} is greater than {secondInt}");
+      }
+      else if(firstInt == secondInt)
+      {
+        Console.WriteLine($"{firstInt} is equal to {secondInt}");
+      }
+      else
+      {
+        Console.WriteLine($"{firstInt} is smaller than {secondInt}");
+      }
+
+      switch(DateAndTime.Today.DayOfWeek)
+      {
+        case DayOfWeek.Saturday:
+        case DayOfWeek.Sunday:
+          Console.WriteLine("It is weekend");
+          break;
+        default:
+          Console.WriteLine("It is a weekday".ToLower());
+          break;
+      }
+      
+
+      // Shorthand
+      string comparison = firstInt > secondInt ? "greater than" : firstInt < secondInt ? "smaller than" : "equal to";
+      Console.WriteLine($"{firstInt} is {comparison} {secondInt}");
 
     }
   }
--- obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
+++ obj/Debug/net8.0/HelloWorld.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+12a52fc5ac279201301fec3b4a3b934e9dd3c826")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7b35c57f123ae471fe5f5666d2ec37b8de71125c")]
 [assembly: System.Reflection.AssemblyProductAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyTitleAttribute("HelloWorld")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
