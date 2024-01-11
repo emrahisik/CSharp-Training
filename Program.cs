f83b2e4c1601ae47dc80fe1d23553ddcbc7f753d
emrahisik <emrahisk@gmail.com>
2024-01-10 23:05:47 -0500

    Operators and Comparisons

--- Program.cs
+++ Program.cs
@@ -1,7 +1,5 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using System;
-using System.Diagnostics;
-using Microsoft.VisualBasic;
 namespace HelloWorld
 {
   internal class Program
@@ -10,41 +8,36 @@ namespace HelloWorld
     {
       int firstInt = 5;
       int secondInt = 10;
-      // Console.WriteLine(firstInt.Equals(secondInt)); // returns true || flase 
-      // Console.WriteLine(firstInt.CompareTo(secondInt)); // returns 0 (equal)|| 1 (greater than) || -1 (less than)
-      // //Shoerthand
-      // Console.WriteLine(firstInt == secondInt);
-      // Console.WriteLine(firstInt < secondInt);
-      
-      if(firstInt > secondInt)
-      {
-        Console.WriteLine($"{firstInt} is greater than {secondInt}");
-      }
-      else if(firstInt == secondInt)
-      {
-        Console.WriteLine($"{firstInt} is equal to {secondInt}");
-      }
-      else
-      {
-        Console.WriteLine($"{firstInt} is smaller than {secondInt}");
-      }
+      int resOne = firstInt++;
+      Console.WriteLine(firstInt);
+      int resTwo = ++firstInt;
+      Console.WriteLine(resOne);
+      Console.WriteLine(resTwo);
 
-      switch(DateAndTime.Today.DayOfWeek)
+      Console.WriteLine(Math.Pow(5,4));
+      Console.WriteLine(Math.Sqrt(5)==Math.Pow(5,0.5));
+      Console.WriteLine(Math.Max(34,4));
+
+      string test = "test";
+      Console.WriteLine(test);
+
+      test += " concatinate";
+      Console.WriteLine(test);
+
+      string quote = " \"third\""; // Backslash to escape from quote error
+      test += quote;
+      Console.WriteLine(test);
+
+      string[] testArray = test.Split(" ");
+      for (int i = 0; i < testArray.Length; i++)
       {
-        case DayOfWeek.Saturday:
-        case DayOfWeek.Sunday:
-          Console.WriteLine("It is weekend");
-          break;
-        default:
-          Console.WriteLine("It is a weekday".ToLower());
-          break;
+        Console.WriteLine($"{testArray[i]}");
       }
-      
-
-      // Shorthand
-      string comparison = firstInt > secondInt ? "greater than" : firstInt < secondInt ? "smaller than" : "equal to";
-      Console.WriteLine($"{firstInt} is {comparison} {secondInt}");
-
+      Console.WriteLine(firstInt.Equals(secondInt)); // returns true || flase 
+      Console.WriteLine(firstInt.CompareTo(secondInt)); // returns 0 (equal)|| 1 (greater than) || -1 (less than)
+      //Shoerthand
+      Console.WriteLine(firstInt == secondInt);
+      Console.WriteLine(firstInt < secondInt);
     }
   }
 }
